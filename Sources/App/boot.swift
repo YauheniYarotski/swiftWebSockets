2a65c66d08535bfa18974696a3bce75e785fb0ca
Yauheni Yarotski <yauheni_yarotski@me.com>
2019-03-22 17:19:38 +0300

    init ws

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,6 +1,64 @@
 import Vapor
+import WebSocket
+//import HTTP
 
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // Your code here
+  let worker = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+  // Create a new WebSocket connected to echo.websocket.org
+//  let ws = try HTTPClient.webSocket(scheme: .wss, hostname: "api.bitfinex.com/ws/2", on: worker).wait()
+  let ws = try HTTPClient.webSocket(scheme: .wss, hostname: "api.bitfinex.com", path: "/ws/2", on: worker).wait()
+  
+//  let promise = worker.eventLoop.newPromise(String.self)
+  
+  // Set a new callback for receiving text formatted data.
+  ws.onText { ws, text in
+//    ws.m
+    print("Server echo: \(text)")
+  }
+  
+  ws.onBinary { (ws, data) in
+    print("Data: \(data)")
+  }
+  
+  // Send a message.
+//  ws.send("Hello, world!!!!")
+//  ws.d
+//  let collection = ["event":"ping", "cid": 1234] as [String : Any]
+  
+  let tickerRequest: [String : Any] = ["event":"subscribe", "channel":"trades", "symbol":"tBTCUSD"]
+  
+  let theJSONData = try!  JSONSerialization.data(
+    withJSONObject: tickerRequest,
+    options: .prettyPrinted
+    )
+//    let theJSONText = String(data: theJSONData,
+//                             encoding: String.Encoding.ascii)!
+//    print("JSON string = \n\(theJSONText)")
+  
+
+//  ws.send(theJSONText)
+  ws.send(theJSONData)
+  
+  // Wait for the Websocket to close.
+  try ws.onClose.wait()
 }
+
+
+//extension WebSocket {
+//  func send(_ json: JSON) throws {
+//    let js = json.serialize(.PrettyPrint)
+//    try send(js)
+//  }
+//}
+
+
+//let user: User ...
+//// Encode JSON using custom date encoding strategy
+//try req.content.encode(json: user, using: .custom(dates: .millisecondsSince1970))
+
+
+//struct User: Encodable {
+//  <#fields#>
+//}
